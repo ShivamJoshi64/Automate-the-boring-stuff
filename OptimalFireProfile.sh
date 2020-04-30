ce793950a6696044f9f25a295b7e5cb35eca682e
Shivam Joshi <joshishivam64@gmail.com>
2020-04-30 19:59:40 -0400

    firefox session automation

---
+++ OptimalFireProfile.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+firefox --new-window https://github.com/udacity/deep-reinforcement-learning 
+firefox https://github.com/righetti/optlearningcontrol
+firefox https://www.youtube.com/watch?v=_84zsIol2UA
+firefox https://www.youtube.com/watch?v=wlkRYMVUZTs
+
+firefox https://en.wikipedia.org/wiki/Trajectory_optimization
