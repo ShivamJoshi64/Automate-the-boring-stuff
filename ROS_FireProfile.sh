c09919b6d0b3a6527c856f1d9cd6fad1210baf42
Shivam Joshi <joshishivam64@gmail.com>
2020-05-27 02:11:31 -0400

    new files

---
+++ ROS_FireProfile.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+firefox --new-window https://rsl.ethz.ch/education-students/lectures/ros.html 
+
+firefox https://sce.ethz.ch/en/programmes-and-courses/angebot-nach-programmart/e-learning.html
+firefox https://www.youtube.com/playlist?list=PLuteWQUGtU9BU0sQIVqRQa24p-pSBCYNv
+firefox https://www.youtube.com/watch?v=1tqYrWqrbC8&list=PLRG6WP3c31_U7TFGduEIJWVtkOw6AJjFf&index=2&t=0s
+firefox http://wiki.ros.org/actionlib_tutorials/Tutorials	
---
+++ RaspberryPiFireProfile.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+firefox --new-window https://github.com/udacity/deep-reinforcement-learning 
+
+firefox https://www.youtube.com/user/DrZAP42/playlists
+firefox https://www.youtube.com/playlist?list=PL5--8gKSku174EnRTbP4DzU2W80Q1vqtm
+firefox https://www.youtube.com/results?search_query=fire+detecting+robot+raspberry+pi+3+camera
